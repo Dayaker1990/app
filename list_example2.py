9d8f0a4ed9923d3b86e7f5a60eef40cca6148fe5
Dayaker1990 <dayakerdaya90@gmail.com>
2024-06-18 15:50:34 +0530

    Add files via upload

---
+++ list_example2.py
@@ -0,0 +1,10 @@
+thislist=["apple", "banana", "orange"]
+thislist.append("cherry")
+print(thislist)
+thislist.remove("cherry")
+print(thislist)
+thislist.pop(1)
+print(thislist)
+del thislist[0]
+print(thislist)
+    q
