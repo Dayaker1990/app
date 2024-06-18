de7d26674d89ef4ab3a0704229a62f73cdad80ac
Dayaker1990 <dayakerdaya90@gmail.com>
2024-06-18 16:37:56 +0530

    Add files via upload

---
+++ list_example4.py
@@ -0,0 +1,10 @@
+thislist=["sun", "moon","earth"]
+for i in range (len(thislist)):
+    print(thislist[i])
+thislist=[3,2,5,8,10,20,30,40,50,80]
+thislist.sort()
+print(thislist)
+thislist.sort(reverse=True)
+print(thislist)
+mylist=thislist.copy()
+print(mylist)
