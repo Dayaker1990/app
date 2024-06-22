aca497656522544191ec325997e4cfa5a367d00e
Dayaker1990 <dayakerdaya90@gmail.com>
2024-06-22 23:29:58 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+#titlle
+st.title('Python Station')
+#header
+st.header('Streamlit Tutorial')
+#Subheader
+st.subheader('Complete Streamlit Tutorial')
+#text
+st.text('''hello everyone 
+welcome to my python station streamlit web App
+Hope you like it...''')
+#write
+st.write(' All are please attend for the classes')
+#colourful text
+#success
+st.success('Success')
+#error
+st.error('Error')
+#warning
+st.warning('Warning')
+#info
+st.info('Info')
+#exception
+st.exception('Name error(name is not define)')
+#help
+st.help(help)
+
+
+
+
+
